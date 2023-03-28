a34acc6ee69e578dca5319daac5cbcd386a453b6
Sandeep <sandeepkrishnath54@gmail.com>
2023-03-28 23:40:30 +0530

    End of Session 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace API.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<AppUser> Users {get; set;}
+    }
+}
