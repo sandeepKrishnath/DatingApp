9ce3ff59da0aef25cb8dbee81e54bec828343710
Sandeep <sandeepkrishnath54@gmail.com>
2023-04-08 20:56:45 +0530

    End of session 6 - Routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,9 +8,13 @@ import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { SharedModule } from './_modules/shared.module';
 
 
 @NgModule({
@@ -18,7 +22,11 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    ListsComponent,
+    MessagesComponent,
+    MemberDetailComponent,
+    MemberListComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +34,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -12,14 +13,14 @@ export class RegisterComponent {
   @Output() cancelRegister =  new EventEmitter<boolean>()
   model: any = {}
 
-  constructor(private accountService: AccountService) {
+  constructor(private accountService: AccountService, private toastr : ToastrService) {
     
   }
 
   register(){
     this.accountService.register(this.model).subscribe({
        next: () => this.cancel(),
-       error: (error) => console.log(error)
+       error: (error) => this.toastr.error(error.error)
     })
   }
 
